590c62c58fd1ea7d99081262cf9167c9aeac0d00
fntnhd <s4m6a6@gmail.com>
2015-11-29 21:50:52 -0800

    Fixed glue code package name.

--- src/test/java/org/thezenofcode/billing/at/AccountManagementTest.java
+++ src/test/java/org/thezenofcode/billing/at/AccountManagementTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
  * Runs the Cucumber tests
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "json:target/cucumber.json", "html:target/site/cucumber-pretty"}, features= {"classpath:features"}, glue = {"com.example.billing.at.steps"})
+@CucumberOptions(plugin = {"pretty", "json:target/cucumber.json", "html:target/site/cucumber-pretty"}, features= {"classpath:features"}, glue = {"org.thezenofcode.billing.at.steps"})
 public class AccountManagementTest {
 
 }
